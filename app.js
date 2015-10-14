cc55d25a965d71e65096ff198a5f658e7fd456fb
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2015-10-14 10:10:40 +0530

    Updated port number

--- app.js
+++ app.js
@@ -246,7 +246,7 @@ app.post('/parsefeedurl', function (req, res) {
 
 });
 
-var server = app.listen(3020, function () {
+var server = app.listen(process.env.PORT || 3020, function () {
   var host = server.address().address;
   var port = server.address().port;
 
